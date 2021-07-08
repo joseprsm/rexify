b5d0c9281f8ab4b63569b807b1174f9cc0971e88
José Medeiros <joseprsm@gmail.com>
2021-07-08 18:46:04 +0100

    Improved recommender coverage

--- tests/test_recommender.py
+++ tests/test_recommender.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import tensorflow as tf
 
@@ -5,7 +6,11 @@ from rexify.recommender import Recommender
 
 
 def _load_mock_data():
-    events = pd.read_csv('data/events.csv').head(1024)
+    events = pd.DataFrame({
+        'userId': np.random.choice(
+            list(range(1, 31)), 1024),
+        'itemId': np.random.choice(
+            list(range(67, 95)), 1024)})
 
     def add_header(x):
         return {events.columns[i]: x[i] for i in range(len(events.columns))}
@@ -15,11 +20,29 @@ def _load_mock_data():
         map(add_header)
 
 
-def test_model_training():
+def test_recommender_training():
     events = _load_mock_data()
 
-    rex = Recommender(250_000, 10_000)
+    rex = Recommender(50, 50)
     rex.compile(optimizer=tf.keras.optimizers.Adagrad(0.2))
+    assert rex.history is None
 
     rex.fit(events.batch(512))
     assert rex.history is not None
+
+
+def test_recommender_call():
+    rex = Recommender(50, 50)
+    sample_query = {'userId': tf.constant([1]), 'itemId': tf.constant([1])}
+    query_embeddings, candidate_embeddings = rex(sample_query)
+    assert isinstance(query_embeddings, tf.Tensor)
+    assert isinstance(candidate_embeddings, tf.Tensor)
+
+    assert query_embeddings.shape == tf.TensorShape([1, 32])
+    assert candidate_embeddings.shape == tf.TensorShape([1, 32])
+
+
+def test_recommender_get_config():
+    rex = Recommender(50, 50)
+    assert isinstance(rex.get_config(), dict)
+    assert rex.get_config() == dict()
