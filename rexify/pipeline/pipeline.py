d9ee48658b85bce4b2419162b7ebc7d3a923a63f
José Medeiros <joseprsm@gmail.com>
2021-10-09 00:04:51 +0100

    refactor pipeline definition

--- rexify/pipeline/pipeline.py
+++ rexify/pipeline/pipeline.py
@@ -1,48 +1,47 @@
-from typing import List, Text, Dict
+from typing import List, Dict, Optional
 
 import json
 
+import tfx
+import tfx.proto
+
 from tfx.orchestration.pipeline import Pipeline
 from tfx.orchestration.metadata import sqlite_metadata_connection_config
-from tfx.components import CsvExampleGen, Trainer, Pusher
 from tfx.components.trainer import executor as trainer_executor
 from tfx.dsl.components.base.base_component import BaseComponent
 from tfx.dsl.components.base import executor_spec
-from tfx.proto import trainer_pb2
-from tfx.proto.pusher_pb2 import PushDestination
 
-from rexify.pipeline.components import LookupGen, ScaNNGen
+from rexify.pipeline import components as rexify_components
 
 
-def build(
-        pipeline_name: Text,
-        pipeline_root: Text,
-        data_root: Text,
-        items_root: Text,
-        schema: Dict[Text, Text],
-        run_fn: Text,
-        serving_model_dir: Text,
-        metadata_path: Text
-) -> Pipeline:
+def build(pipeline_name: str,
+          pipeline_root: str,
+          data_root: str,
+          items_root: str,
+          schema: Dict[str, str],
+          run_fn: str,
+          serving_model_dir: str,
+          metadata_path: str,
+          enable_cache: Optional[bool] = True) -> Pipeline:
 
     components: List[BaseComponent] = list()
 
-    example_gen = CsvExampleGen(input_base=data_root).with_id('event_gen')
-    components.append(example_gen)
+    event_gen = tfx.components.CsvExampleGen(input_base=data_root).with_id('EventGen')
+    components.append(event_gen)
 
     trainer_args = dict(
         run_fn=run_fn,
-        examples=example_gen.outputs.examples,
-        train_args=trainer_pb2.TrainArgs(num_steps=1000),
-        eval_args=trainer_pb2.EvalArgs(num_steps=1000),
+        examples=event_gen.outputs.examples,
+        train_args=tfx.proto.trainer_pb2.TrainArgs(num_steps=1000),
+        eval_args=tfx.proto.trainer_pb2.EvalArgs(num_steps=1000),
         custom_executor_spec=executor_spec.ExecutorClassSpec(trainer_executor.GenericExecutor))
-    trainer = Trainer(**trainer_args)
+    trainer = tfx.components.Trainer(**trainer_args)
     components.append(trainer)
 
-    item_gen = CsvExampleGen(input_base=items_root).with_id('item_gen')
+    item_gen = tfx.components.CsvExampleGen(input_base=items_root).with_id('ItemGen')
     components.append(item_gen)
 
-    lookup_gen = LookupGen(
+    lookup_gen = rexify_components.LookupGen(
         examples=item_gen.outputs.examples,
         model=trainer.outputs.model,
         query_model='candidate_model',
@@ -50,7 +49,7 @@ def build(
         schema=json.dumps(schema))
     components.append(lookup_gen)
 
-    scann_gen = ScaNNGen(
+    scann_gen = rexify_components.ScaNNGen(
         candidates=item_gen.outputs.examples,
         model=trainer.outputs.model,
         lookup_model=lookup_gen.outputs.lookup_model,
@@ -60,14 +59,15 @@ def build(
 
     pusher_args = dict(
         model=scann_gen.outputs.index,
-        push_destination=PushDestination(
-            filesystem=PushDestination.Filesystem(
+        push_destination=tfx.proto.pusher_pb2.PushDestination(
+            filesystem=tfx.proto.pusher_pb2.PushDestination.Filesystem(
                 base_directory=serving_model_dir)))
-    pusher = Pusher(**pusher_args)
+    pusher = tfx.components.Pusher(**pusher_args)
     components.append(pusher)
 
     return Pipeline(
         pipeline_name=pipeline_name,
         pipeline_root=pipeline_root,
         components=components,
+        enable_cache=enable_cache,
         metadata_connection_config=sqlite_metadata_connection_config(metadata_path))
