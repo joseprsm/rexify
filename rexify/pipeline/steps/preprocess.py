3312cc2c9f61aac60e1661c135a9cbc5ea6e168f
José Medeiros <joseprsm@gmail.com>
2021-10-27 00:13:47 +0100

    add preprocessing step draft

---
+++ rexify/pipeline/steps/preprocess.py
@@ -0,0 +1,35 @@
+from typing import List, Dict
+
+import tensorflow as tf
+import tensorflow_transform as tft
+
+CATEGORICAL_FEATURE_KEYS: List[str] = ...
+NUMERIC_FEATURE_KEYS: List[str] = ...
+DATE_KEY: str = ...
+
+NUM_OOV_BUCKETS = 1
+
+
+def preprocessing_fn(inputs: Dict[str, tf.Tensor]):
+
+    schema = ...  # todo: read from custom config
+    outputs = {}
+
+    for key in NUMERIC_FEATURE_KEYS:
+        outputs[key] = tft.scale_to_0_1(inputs[key])
+
+    for key in CATEGORICAL_FEATURE_KEYS:
+        outputs[key] = tft.compute_and_apply_vocabulary(
+            tf.strings.strip(inputs[key]),
+            num_oov_buckets=NUM_OOV_BUCKETS,
+            vocab_filename=key)
+
+    # todo: add cyclical transform for date features
+
+    # passing remaining features as they will be transformed during training
+    # todo: add schema validation
+    for key in inputs.keys():
+        if key not in CATEGORICAL_FEATURE_KEYS + NUMERIC_FEATURE_KEYS and key in schema.keys():
+            outputs[key] = inputs[key]
+
+    return outputs
