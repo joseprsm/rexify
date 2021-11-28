0be5e45302cc059b5e56fd36dc1fc5e07cce195b
José Medeiros <joseprsm@gmail.com>
2021-11-28 15:55:47 +0000

    flat schema input

--- rexify/pipeline/steps/preprocess.py
+++ rexify/pipeline/steps/preprocess.py
@@ -1,11 +1,7 @@
-from typing import List, Dict, Any
+from typing import Dict, Any
 
 import tensorflow as tf
 import tensorflow_transform as tft
-
-CATEGORICAL_FEATURE_KEYS: List[str] = ...
-NUMERIC_FEATURE_KEYS: List[str] = ...
-DATE_KEY: str = ...
 
 NUM_OOV_BUCKETS = 1
 
@@ -14,12 +10,25 @@ def preprocessing_fn(inputs: Dict[str, tf.Tensor],
                      custom_config: Dict[str, Any]):
 
     schema = custom_config['schema']
+    flat_schema = schema['user']
+    flat_schema.update(schema['item'])
+    flat_schema.update(schema['context'])
+
+    feature_keys = {
+        dtype: [key for key, value in flat_schema.items() if value == dtype]
+        for dtype in set(flat_schema.values())
+    }
+
+    categorical_feature_keys = feature_keys.pop('categorical')
+    numeric_feature_keys = feature_keys.pop('numeric')
+    # date_feature_key = feature_keys.pop('timestamp')
+
     outputs = {}
 
-    for key in NUMERIC_FEATURE_KEYS:
+    for key in numeric_feature_keys:
         outputs[key] = tft.scale_to_0_1(inputs[key])
 
-    for key in CATEGORICAL_FEATURE_KEYS:
+    for key in categorical_feature_keys:
         outputs[key] = tft.compute_and_apply_vocabulary(
             tf.strings.strip(inputs[key]),
             num_oov_buckets=NUM_OOV_BUCKETS,
@@ -30,7 +39,7 @@ def preprocessing_fn(inputs: Dict[str, tf.Tensor],
     # passing remaining features as they will be transformed during training
     # todo: add schema validation
     for key in inputs.keys():
-        if key not in CATEGORICAL_FEATURE_KEYS + NUMERIC_FEATURE_KEYS and key in schema.keys():
+        if key not in categorical_feature_keys + numeric_feature_keys and key in flat_schema.keys():
             outputs[key] = inputs[key]
 
     return outputs
