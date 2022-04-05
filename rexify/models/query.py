87da19a2d07cfcac53cc4dfc5f5fc377975cf401
José Medeiros <joseprsm@gmail.com>
2022-04-05 11:52:05 +0100

    remove sequential model

--- rexify/models/query.py
+++ rexify/models/query.py
@@ -3,17 +3,13 @@ from typing import Dict, List, Any
 import tensorflow as tf
 
 from rexify.models.tower import Tower
-from rexify.models.embedding import SequenceModel
 
 
 class QueryModel(Tower):
 
     def __init__(self,
-                 candidate_model: tf.keras.Model,
                  schema: Dict[str, str],
                  params: Dict[str, Dict[str, Any]],
-                 recurrent_layers: List[int],
-                 layer_type: str,
                  layer_sizes: List[int],
                  activation: str = 'relu'):
 
@@ -24,10 +20,6 @@ class QueryModel(Tower):
             activation=activation)
 
         self.user_model = self.feature_models.pop('userId')
-        self.sequence_model = SequenceModel(
-            candidate_model=candidate_model,
-            layer_type=layer_type,
-            layer_sizes=recurrent_layers)
 
     def call_feature_models(self, inputs: Dict[str, tf.Tensor]) -> List[tf.Tensor]:
         user_embeddings = self.user_model(inputs['userId'])
