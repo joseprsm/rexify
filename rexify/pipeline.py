bce6a2e03bc7e904483ac65b1b62cc1b45cb69fc
José Medeiros <joseprsm@gmail.com>
2022-05-03 15:09:35 +0100

    add sample steps. mount pvc to pipeline tasks

--- rexify/pipeline.py
+++ rexify/pipeline.py
@@ -2,6 +2,7 @@ import os
 import kfp
 
 from kfp.components import load_component_from_file
+from kfp.onprem import mount_pvc
 
 BASE_PATH = os.path.join("rexify", "components")
 KFP_HOST = os.environ.get("KFP_HOST", "http://localhost:3000")
@@ -13,14 +14,25 @@ def _load_component(task: str):
 
 preprocess_op = _load_component("preprocess")
 train_op = _load_component("train")
+index_op = _load_component("index")
+deploy_op = _load_component("deploy")
 
 
 # noinspection PyUnusedLocal
 @kfp.dsl.pipeline()
 def pipeline_fn():
     preprocess_task = preprocess_op()
-    train_task = train_op()
+    preprocess_task.apply(mount_pvc('rexify-pvc', 'data-vol', '/mnt/data'))
+
+    train_task = train_op(input_dir=preprocess_task.outputs["output_dir"])
+    train_task.apply(mount_pvc('rexify-pvc', 'data-vol', '/mnt/data'))
+
+    index_task = index_op(model_dir=train_task.outputs["model_dir"])
+    index_task.apply(mount_pvc('rexify-pvc', 'data-vol', '/mnt/data'))
+
+    deploy_task = deploy_op(index_dir=index_task.outputs["index_dir"])
 
 
-client = kfp.Client(host=KFP_HOST)
-client.create_run_from_pipeline_func(pipeline_fn, arguments={})
+if __name__ == '__main__':
+    client = kfp.Client(host=KFP_HOST)
+    client.create_run_from_pipeline_func(pipeline_fn, arguments={})
