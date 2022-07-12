8a91717b960740d936f589388b296b15cb27d9a0
José Medeiros <joseprsm@gmail.com>
2022-07-13 00:17:59 +0100

    add cyclical transformer

---
+++ rexify/features/cyclical.py
@@ -0,0 +1,26 @@
+from typing import Optional, Callable
+
+import numpy as np
+import pandas as pd
+
+from rexify.features.base import BaseTransformer
+
+
+class CyclicalTransformer(BaseTransformer):
+    def __init__(
+        self, num_bins: int = 24, fn: Optional[str] = "sin", drop_columns: bool = True
+    ):
+        super().__init__()
+        self.num_bins = num_bins
+        self.drop_columns = drop_columns
+        self.fn = fn
+        self.fn_ = getattr(np, fn)
+
+    def transform(self, X):
+        transformed = self.fn_((2.0 * X * np.pi / self.num_bins))
+        transformed = (
+            np.concatenate([X, transformed], axis=1)
+            if not self.drop_columns
+            else transformed
+        )
+        return transformed
