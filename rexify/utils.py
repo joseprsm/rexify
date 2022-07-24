1a1320372b102f80d50c493873ead52c77d3ff7a
José Medeiros <joseprsm@gmail.com>
2022-07-24 19:09:37 +0100

    add get_target_id function to utils

--- rexify/utils.py
+++ rexify/utils.py
@@ -3,3 +3,10 @@ from typing import Any, List
 
 def flatten(xss: List[List[Any]]):
     return [x for xs in xss for x in xs]
+
+
+def get_target_id(schema: dict, target: str):
+    def mask(x: tuple):
+        return x[1] == "id"
+
+    return list(filter(mask, schema[target].items()))[0][0]
