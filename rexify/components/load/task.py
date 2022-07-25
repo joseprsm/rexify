b3c5c154281505af98a92c5f54fddc5d248d5777
José Medeiros <joseprsm@gmail.com>
2022-07-25 22:43:06 +0100

    add missing argument

--- rexify/components/load/task.py
+++ rexify/components/load/task.py
@@ -15,6 +15,7 @@ from rexify.utils import flatten, get_target_id
 @click.option("--events-path", type=str)
 @click.option("--schema-path", type=str)
 @click.option("--items-dir", type=str)
+@click.option("--users-dir", type=str)
 @click.option("--train-data-dir", type=str)
 @click.option("--test-data-dir", type=str)
 @click.option("--test-size", type=float, default=0.3)
@@ -48,26 +49,26 @@ def load(
     Path(train_data_dir).mkdir(parents=True, exist_ok=True)
     Path(test_data_dir).mkdir(parents=True, exist_ok=True)
 
-    train_path = Path(train_data_dir) / 'train.csv'
-    test_path = Path(test_data_dir) / 'test.csv'
+    train_path = Path(train_data_dir) / "train.csv"
+    test_path = Path(test_data_dir) / "test.csv"
 
     np.savetxt(train_path, train, delimiter=",")
     np.savetxt(test_path, test, delimiter=",")
 
     transformed_events = ppl.transform(events)
 
-    item_id = get_target_id(schema, 'item')
+    item_id = get_target_id(schema, "item")
     items = transformed_events[:, np.argwhere(events.columns == item_id)[0, 0]]
 
     Path(items_dir).mkdir(parents=True, exist_ok=True)
-    items_path = Path(items_dir) / 'items.csv'
+    items_path = Path(items_dir) / "items.csv"
     np.savetxt(items_path, items)
 
-    user_id = get_target_id(schema, 'user')
+    user_id = get_target_id(schema, "user")
     users = transformed_events[:, np.argwhere(events.columns == user_id)[0, 0]]
 
     Path(users_dir).mkdir(parents=True, exist_ok=True)
-    users_path = Path(users_dir) / 'users.csv'
+    users_path = Path(users_dir) / "users.csv"
     np.savetxt(users_path, users)
 
 
