6315488ce12b4f07e69359ee2c5c9bc5ce8985f6
José Medeiros <joseprsm@gmail.com>
2022-07-25 11:41:53 +0100

    fixed index component paths

--- rexify/components/index/task.py
+++ rexify/components/index/task.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import json
 import click
 import numpy as np
@@ -9,15 +11,15 @@ from rexify.utils import get_target_id
 
 
 @click.command()
-@click.option("--items-path", type=str)
+@click.option("--items-dir", type=str)
 @click.option("--schema-path", type=str)
 @click.option("--model-path", type=str)
-@click.option("--index-path", type=str)
+@click.option("--index-dir", type=str)
 def index(
-    items_path: str,
+    items_dir: str,
     schema_path: str,
-    model_path: str,
-    index_path: str,
+    model_dir: str,
+    index_dir: str,
 ):
     with open(schema_path, "r") as f:
         schema = json.load(f)
@@ -25,11 +27,13 @@ def index(
     user_id = get_target_id(schema, 'user')
     item_id = get_target_id(schema, 'item')
 
+    items_path = Path(items_dir) / 'items.csv'
     items = np.loadtxt(items_path)
     items = tf.data.Dataset.from_tensor_slices(items.reshape(-1, 1)).map(
         lambda x: {item_id: x}
     )
 
+    model_path = Path(model_dir) / 'model'
     model = tf.keras.models.load_model(model_path)
 
     item_embeddings = items.map(model.candidate_model)
@@ -38,6 +42,9 @@ def index(
     scann.index_from_dataset(tf.data.Dataset.zip((items, item_embeddings)))
     _ = scann(model.query_model({user_id: [42]}), k=1)
 
+    index_dir = Path(index_dir)
+    index_dir.mkdir(parents=True, exist_ok=True)
+    index_path = index_dir / 'model'
     tf.keras.models.save_model(
         scann,
         index_path,
