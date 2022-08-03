30d4c439d3d5ca452fde62cae8e4f57f0a381814
José Medeiros <joseprsm@gmail.com>
2022-08-03 18:32:11 +0100

    update theme

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+html_theme = 'sphinx_material'
+
+# Material theme options (see theme.conf for more information)
+html_theme_options = {
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'Rexify',
+
+    # Set the color and the accent color
+    'color_primary': 'blue',
+    'color_accent': 'light-blue',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://github.com/joseprsm/rexify/',
+    'repo_name': 'Rexify',
+
+}
