b6ee2ccf02414143a832f7f7f735eebb1b61b1a2
José Medeiros <joseprsm@gmail.com>
2022-08-04 22:16:40 +0100

    download data instead of uploading

--- rexify/app/dashboard.py
+++ rexify/app/dashboard.py
@@ -1,23 +1,27 @@
 import json
 
+import requests
 import pandas as pd
 import streamlit as st
 
 from rexify.pipeline import compile_
 
-events = schema_uri = None
+events = None
 
 data_tab, schema_tab, pipeline_tab = st.tabs(["Data", "Schema", "Pipeline"])
 
 with data_tab:
 
-    uploaded_file = st.file_uploader("Upload events data")
-    if uploaded_file:
-        events = pd.read_csv(uploaded_file)
+    event_url = st.text_input("Event data URL")
+    if st.button("Download"):
+        response = requests.get(event_url)
+        with open("events.csv", "w") as f:
+            f.write(response.text)
 
-    events_uri = st.text_input("Event data URL")
-    st.button("Upload")
+    events = pd.read_csv("events.csv")
 
+    if events is not None:
+        st.dataframe(events)
 
 with schema_tab:
     schema = dict(user=dict(), item=dict())
@@ -55,18 +59,13 @@ with schema_tab:
         with col2:
             st.code(json.dumps(schema, indent=2))
 
-        if (len(user_feats) != 0) and (len(item_feats) != 0):
-            upload_schema_disabled = False
-
-        schema_uri = st.text_input("Schema upload location")
-        st.button("Upload schema", disabled=upload_schema_disabled)
 
 with pipeline_tab:
 
-    if (events_uri is not None) and (schema_uri is not None):
+    if (event_url is not None) and (schema is not None):
         if st.button(
             "Compile pipeline",
-            on_click=compile_(event_uri=events_uri, schema_uri=schema_uri),
+            on_click=compile_(event_uri=event_url, schema=schema),
         ):
 
             with open("pipeline.json") as f:
