b72822d49f93a82f1997e91e70c0ca209b5bdafa
José Medeiros <joseprsm@gmail.com>
2022-08-08 16:42:41 +0100

    refactor schema task, add epochs arg

--- rexify/pipeline.py
+++ rexify/pipeline.py
@@ -22,35 +22,40 @@ retrieval_op = _load_component("retrieval")
 
 
 @pipeline(name=PIPELINE_NAME, pipeline_root=PIPELINE_ROOT)
-def pipeline_fn(event_uri: str = None, schema_uri: str = None, schema: dict = None):
-    events_downloader_task = download_op(
-        input_uri=event_uri or os.environ.get("INPUT_DATA_URL")
-    )
+def pipeline_fn(
+    event_uri: str = None,
+    schema_uri: str = None,
+    schema: dict = None,
+    epochs: int = 100,
+):
+    events_task = download_op(input_uri=event_uri or os.environ.get("INPUT_DATA_URL"))
 
-    schema_downloader_task = (
+    schema_task = (
         download_op(input_uri=schema_uri or os.environ.get("SCHEMA_URL"))
         if schema is None
         else copy_op(content=schema)
     )
 
     load_task = load_op(
-        events=events_downloader_task.outputs["data"],
-        schema=schema_downloader_task.outputs["data"],
+        events=events_task.outputs["data"],
+        schema=schema_task.outputs["data"],
     )
 
     train_task = train_op(
-        train_data=load_task.outputs["train"], feat=load_task.outputs["feat"]
+        train_data=load_task.outputs["train"],
+        feat=load_task.outputs["feat"],
+        epochs=epochs,
     )
 
     index_task = index_op(
         items=load_task.outputs["items"],
         model=train_task.outputs["model"],
-        schema=schema_downloader_task.outputs["data"],
+        schema=schema_task.outputs["data"],
     )
 
     retrieval_task = retrieval_op(
         users=load_task.outputs["users"],
-        schema=schema_downloader_task.outputs["data"],
+        schema=schema_task.outputs["data"],
         index=index_task.outputs["index"],
         model=train_task.outputs["model"],
     )
