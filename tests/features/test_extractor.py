eb8fcb8a568a8a374637bfefb796eec223b54e48
José Medeiros <joseprsm@gmail.com>
2022-08-18 14:29:31 +0100

    add header tests to FeatureExtractor

--- tests/features/test_extractor.py
+++ tests/features/test_extractor.py
@@ -4,6 +4,7 @@ from typing import Callable
 
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 
 from rexify.features import FeatureExtractor
 
@@ -76,12 +77,67 @@ def test_dataset_main_keys():
     assert np.all(np.in1d(list(out.keys()), TOWERS))
 
 
-def test_dataset_header_output():
+def test_dataset_header_output_no_features():
     assert isinstance(feat._get_header_fn(), Callable)
     assert feat._get_header_fn()(np.array([1, 2])) == {
         "query": {"user_id": 1},
         "candidate": {"item_id": 2},
     }
+
+
+def test_dataset_header_output_with_features():
+    schema_ = {
+        "user": {"user_id": "id", "gender": "categorical", "is_happy": "categorical"},
+        "item": {"item_id": "id", "duration": "numerical", "topic": "categorical"},
+        "context": {"event_type": "categorical"},
+    }
+
+    events_ = events.copy()
+    events_[["gender", "is_happy", "duration", "topic", "event_type"]] = np.concatenate(
+        [
+            np.random.randint(0, 1, size=events.shape[0]).reshape(-1, 1),
+            np.random.randint(0, 1, size=events.shape[0]).reshape(-1, 1),
+            np.random.randint(0, 1_000, size=events.shape[0]).reshape(-1, 1),
+            np.random.randint(0, 5, size=events.shape[0]).reshape(-1, 1),
+            np.zeros(events.shape[0]).reshape(-1, 1),
+        ],
+        axis=1,
+    )
+
+    feat_ = FeatureExtractor(schema=schema_)
+    feat_.fit(events_)
+
+    header = feat_._get_header_fn()(tf.constant([1, 1, 1, 1, 1, 1, 1]))
+
+    expected_result = {
+        "query": {
+            "user_id": tf.constant(1),
+            "user_features": tf.constant([1, 1]),
+            "context_features": tf.constant([0]),
+        },
+        "candidate": {
+            "item_id": tf.constant(1),
+            "item_features": tf.constant([1, 1, 1, 0, 0, 0]),
+        },
+    }
+
+    assert tf.reduce_all(
+        header["query"]["user_features"] == expected_result["query"]["user_features"]
+    )
+    assert tf.reduce_all(
+        header["query"]["context_features"]
+        == expected_result["query"]["context_features"]
+    )
+    assert tf.reduce_all(
+        header["query"]["user_id"] == expected_result["query"]["user_id"]
+    )
+    assert tf.reduce_all(
+        header["candidate"]["item_features"]
+        == expected_result["candidate"]["item_features"]
+    )
+    assert tf.reduce_all(
+        header["candidate"]["item_id"] == expected_result["candidate"]["item_id"]
+    )
 
 
 def test_dataset_candidate_keys():
