20c0c24053a9a212fb510f814350ff616b8cdbe6
José Medeiros <joseprsm@gmail.com>
2022-09-02 17:27:17 +0100

    add more testing combinations. remove fit tests

--- tests/models/test_recommender.py
+++ tests/models/test_recommender.py
@@ -1,5 +1,4 @@
 import itertools
-from tempfile import mkdtemp
 
 import numpy as np
 import pytest
@@ -9,7 +8,7 @@ from rexify import Recommender
 from rexify.utils import flatten
 
 
-SAMPLE_MODEL_PARAMS = ("user_id", 15, "item_id", 15)
+BASE_MODEL_PARAMS = ["user_id", 15, "item_id", 15]
 
 
 def get_base_inputs():
@@ -20,6 +19,7 @@ def get_base_inputs():
             np.random.randint(0, 1_000, size=100).reshape(-1, 1),
             np.random.randint(0, 1_000, size=100).reshape(-1, 1),
             np.random.randint(0, 15, size=100).reshape(-1, 1),
+            np.random.randint(0, 5, size=100).reshape(-1, 1),
             np.random.randint(0, 5, size=100).reshape(-1, 1),
         ],
         axis=1,
@@ -36,10 +36,11 @@ def get_base_dataset():
                     "user_features": x[1:3],
                     "context_features": x[3:4],
                 },
-                "candidate": {"item_id": x[4], "item_features": x[5:]},
+                "candidate": {"item_id": x[4], "item_features": x[5:6]},
+                "rank": x[6:],
             }
         )
-        .batch(16)
+        .batch(32)
     )
 
 
@@ -59,13 +60,16 @@ def get_dataset_missing_features(missing_feature: list[str] = None):
                 },
                 "candidate": {
                     "item_id": x[4],
-                    "item_features": x[5:]
+                    "item_features": x[5:6]
                     if "item_features" not in missing_feature
                     else tf.constant([], dtype=tf.float32),
                 },
+                "rank": x[6:]
+                if "rank" not in missing_feature
+                else tf.constant([], dtype=tf.float32),
             }
         )
-        .batch(16)
+        .batch(32)
     )
 
 
@@ -75,52 +79,52 @@ datasets = [get_base_dataset()] + [
         [
             list(
                 itertools.combinations(
-                    ["user_features", "context_features", "item_features"], i
+                    ["user_features", "context_features", "item_features", "rank"], i
                 )
             )
-            for i in range(1, 4)
+            for i in range(1, 5)
         ]
     )
 ]
 
 
 def get_model_params():
-    return [
-        ("string", 9, "string", 9, 16, [64, 32], [64, 32, 16]),
-        ("string", 9, "string", 9, 8, [32], [64]),
-        # (12, 9, 'string', 9, 8, [32], [64])
+    embedding_dims = [16, 8]
+    feature_layers = [None]
+    output_layers = [None]
+
+    ranking_features = [["rating"], None]
+    ranking_layers = [[32, 16], None]
+    ranking_weights = [None]
+
+    model_params = [
+        BASE_MODEL_PARAMS + list(params)
+        for params in itertools.product(
+            embedding_dims,
+            feature_layers,
+            output_layers,
+            ranking_features,
+            ranking_layers,
+            ranking_weights,
+        )
     ]
 
-
-@pytest.mark.parametrize("inputs", datasets)
-def test_call(inputs):
-    model = Recommender(*SAMPLE_MODEL_PARAMS)
-    query_embeddings, candidate_embeddings = model(list(inputs.take(1))[0])
-
-    assert query_embeddings.shape == tf.TensorShape([16, 32])
-    assert candidate_embeddings.shape == tf.TensorShape([16, 32])
+    return model_params
 
 
-@pytest.mark.parametrize("inputs", datasets)
-def test_fit(inputs):
-    model = Recommender(*SAMPLE_MODEL_PARAMS)
-    model.compile()
-    model.fit(inputs, epochs=1)
+args = list(itertools.product(datasets, get_model_params()))
+args = np.array(args, dtype=object)[
+    ~(
+        np.array([list(a[0].take(1))[0]["rank"].shape[-1] == 0 for a in args])
+        & np.array([a[1][-3] is not None for a in args])
+    )
+].tolist()
 
 
-@pytest.mark.parametrize("inputs", datasets)
-def test_compile(inputs):
-    model = Recommender(*SAMPLE_MODEL_PARAMS)
+@pytest.mark.parametrize("inputs,params", args)
+def test_compile(inputs, params):
+    model = Recommender(*params)
     model.compile(optimizer=tf.keras.optimizers.Adam(0.1))
-    model.fit(inputs, epochs=1)
-
-
-@pytest.mark.parametrize("inputs", datasets)
-def test_save(inputs):
-    model = Recommender(*SAMPLE_MODEL_PARAMS)
-    model.compile(optimizer=tf.keras.optimizers.Adam(0.1))
-    model.fit(inputs, epochs=1)
-    model.save(mkdtemp())
 
 
 @pytest.mark.parametrize("model_params", get_model_params())
@@ -128,17 +132,38 @@ def test_init(model_params):
     Recommender(*model_params)
 
 
-@pytest.mark.parametrize("inputs", datasets)
-def test_compute_loss(inputs):
-    model = Recommender(*SAMPLE_MODEL_PARAMS)
+@pytest.mark.parametrize("inputs,params", args)
+def test_call(inputs, params):
+    model = Recommender(*params)
     inputs = list(inputs.take(1))[0]
-    loss = model.compute_loss(inputs)
+    res = model(inputs)
 
-    assert loss.dtype == tf.float32
+    assert type(res) == tuple
+    assert len(res) == 2
+    assert res[0].shape == res[1].shape
+
+    query_embeddings, candidate_embeddings = res
+    assert query_embeddings.shape == tf.TensorShape([32, 32])
+    assert candidate_embeddings.shape == tf.TensorShape([32, 32])
+
+
+@pytest.mark.parametrize("inputs,params", args)
+def test_compute_loss(inputs, params):
+    model = Recommender(*params)
+    inputs = list(inputs.take(1))[0]
+    embeddings = model(inputs)
+    compare_loss = model.get_retrieval_loss(*embeddings)
+
+    total_loss = model.compute_loss(inputs)
+    if params[-3]:
+        compare_loss += model.get_ranking_loss(*embeddings, inputs["rank"])
+
+    assert total_loss.dtype == tf.float32
+    assert total_loss == compare_loss
 
 
 def test_config():
-    model = Recommender(*SAMPLE_MODEL_PARAMS)
+    model = Recommender(*BASE_MODEL_PARAMS)
     assert model.get_config() == {
         "item_dims": 15,
         "user_dims": 15,
