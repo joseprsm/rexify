cba850eefc5ca5f032ec1ac68b790da29b3a348f
José Medeiros <joseprsm@gmail.com>
2022-09-02 08:22:47 +0100

    add ranking mixin model

--- rexify/models/ranking.py
+++ rexify/models/ranking.py
@@ -1,69 +1,60 @@
+from abc import ABC
+
 import tensorflow as tf
 import tensorflow_recommenders as tfrs
 
-from rexify.models.base import BaseRecommender
 
-
-class RankingModel(BaseRecommender):
-    """
-
-    Args:
-        user_id (str): the user ID feature name
-        user_dims (int): number possible values for the user ID feature
-        item_id (str): the item ID feature name
-        item_dims (int): number possible values for the item ID feature
-        embedding_dim (int): output dimension of the embedding layer
-        output_layers (list): number of neurons in each layer for the output models
-    """
-
+class RankingMixin(tfrs.Model, ABC):
     def __init__(
         self,
-        user_id: str,
-        user_dims: int,
-        item_id: str,
-        item_dims: int,
-        embedding_dim: int = 32,
-        output_layers: list[int] = None,
+        ranking_features: list[str] = None,
+        ranking_layers: list[int] = None,
+        ranking_weights: list[float] = None,
+        **kwargs
     ):
-        super().__init__(
-            user_id=user_id,
-            user_dims=user_dims,
-            item_id=item_id,
-            item_dims=item_dims,
-            embedding_dim=embedding_dim,
-            output_layers=output_layers or [64, 32],
-        )
+        super().__init__()
+        self._ranking_features = ranking_features
+        self._ranking_layers = ranking_layers or [64, 32]
+        self._ranking_weights = ranking_weights
 
-        self.query_model = tf.keras.layers.Embedding(
-            self._user_dims, self._embedding_dim
-        )
-        self.candidate_model = tf.keras.layers.Embedding(
-            self._item_dims, self._embedding_dim
-        )
+        if self._ranking_features:
+            self._ranking_weights = self._ranking_weights or [1] * len(
+                self._ranking_features
+            )
 
-        self.rating_model = self._get_rating_model(self._layer_sizes)
-        self.task = tfrs.tasks.Ranking(
-            loss=tf.keras.losses.MeanSquaredError(),
-            metrics=[tf.keras.metrics.RootMeanSquaredError()],
-        )
+            self.rating_models = {
+                ranking_feature: self._get_rating_model(self._ranking_layers)
+                for ranking_feature in ranking_features
+            }
 
-    def call(self, inputs, *_):
-        user_embedding = self.query_model(inputs[self._user_id])
-        item_embedding = self.candidate_model(inputs[self._item_id])
-        x = tf.concat([user_embedding, item_embedding], axis=1)
-        rating = self.rating_model(x)
-        return rating
+            self.ranking_tasks = {
+                ranking_feature: tfrs.tasks.Ranking(
+                    loss=tf.keras.losses.MeanSquaredError(),
+                    metrics=[tf.keras.metrics.RootMeanSquaredError()],
+                )
+                for ranking_feature in ranking_features
+            }
 
-    def compute_loss(self, inputs, training: bool = False) -> tf.Tensor:
-        labels = inputs.pop("rating")
-        rating_predictions = self(inputs)
-        return self.task(labels=labels, predictions=rating_predictions)
+    def get_ranking_loss(
+        self,
+        query_embeddings: tf.Tensor,
+        candidate_embeddings: tf.Tensor,
+        labels: tf.Tensor,
+    ):
+        loss = 0
+        if self._ranking_features:
+            for i, feature in enumerate(self._ranking_features):
+                rating_model = self.rating_models[feature]
+                x = tf.concat([query_embeddings, candidate_embeddings], axis=1)
+                predictions = rating_model(x)
+                loss += self._ranking_weights[i] * self.ranking_tasks[feature](
+                    labels=labels, predictions=predictions
+                )
+        return loss
 
     @staticmethod
-    def _get_rating_model(layer_sizes: list[int]) -> tf.keras.Model:
-        layers = [
-            tf.keras.layers.Dense(num_neurons, activation="relu")
-            for num_neurons in layer_sizes[:-1]
-        ]
-        layers.append(tf.keras.layers.Dense(1))
+    def _get_rating_model(layer_sizes) -> tf.keras.Model:
+        layers = [tf.keras.layers.Dense(num_neurons) for num_neurons in layer_sizes]
+        if layer_sizes[-1] != 1:
+            layers.append(tf.keras.layers.Dense(1))
         return tf.keras.Sequential(layers)
