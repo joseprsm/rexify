94a2f2f412590f9e823a024b0339c8275a74b2ca
José Medeiros <joseprsm@gmail.com>
2022-10-12 21:33:00 +0100

    add ranking for event types

--- rexify/models/ranking.py
+++ rexify/models/ranking.py
@@ -1,82 +1,69 @@
 from abc import ABC
-from typing import Any
 
 import tensorflow as tf
 import tensorflow_recommenders as tfrs
 
+from rexify.models.base import DenseSetterMixin
 
-class RankingMixin(tfrs.Model, ABC):
+
+class RankingMixin(tfrs.Model, DenseSetterMixin, ABC):
     def __init__(
         self,
-        ranking_features: list[str] = None,
-        ranking_layers: list[int] = None,
-        ranking_weights: dict[str, float] = None,
+        n_dims: int = 1,
+        rating_features: list[str] = None,
+        layer_sizes: list[int] = None,
+        weights: dict[str, float] = None,
     ):
         super().__init__()
-        self._ranking_features = ranking_features
-        self._ranking_layers = ranking_layers or [64, 32]
-        self._ranking_weights = ranking_weights
+        self._n_dims = n_dims
+        self._rating_features = rating_features or []
+        self._layer_sizes = layer_sizes or [64, 32]
+        self._weights = weights
 
-        self.rating_models = self._get_ranking_models()
-        self.ranking_tasks = self._get_ranking_tasks()
+        self.event_model = self._set_dense_layers(self._layer_sizes)
+        self.event_model.add(tf.keras.layers.Dense(self._n_dims, activation="softmax"))
+        self.event_task = tfrs.tasks.Ranking(
+            loss=tf.keras.losses.CategoricalCrossentropy()
+        )
 
-    def get_ranking_loss(
+        if len(self._rating_features) > 0:
+            self.rating_models = self._get_rating_models()
+            self.rating_tasks = self._get_ranking_tasks()
+
+    def get_loss(
         self,
         query_embeddings: tf.Tensor,
         candidate_embeddings: tf.Tensor,
-        event_types: tf.Tensor,
-        ratings: tf.Tensor,
+        events: tf.Tensor,
     ):
-        loss = 0
-        event_types = tf.reshape(event_types, (-1, 1))
         inputs = tf.concat([query_embeddings, candidate_embeddings], axis=1)
-
-        for event_type in self._ranking_features:
-            features, labels = self._filter(inputs, ratings, event_types, event_type)
-            predictions = self.rating_models[event_type](features)
-            loss += 1 * self.ranking_tasks[event_type](
-                labels=labels, predictions=predictions
-            )
-
+        x = self.event_model(inputs)
+        loss = self.event_task(labels=events, predictions=x)
         return loss
 
-    @staticmethod
-    def _filter(
-        features: tf.Tensor,
-        labels: tf.Tensor,
-        condition_tensor: tf.Tensor,
-        value: str | int | Any,
-    ) -> tuple[tf.Tensor, tf.Tensor]:
-        condition = tf.reduce_all(tf.equal(condition_tensor, value), axis=1)
-        indices = tf.where(condition)
-        features = tf.gather_nd(features, indices)
-        labels = tf.gather_nd(labels, indices)
-        return features, labels
-
-    def _get_ranking_models(self) -> dict[str, tf.keras.Model] | None:
-        ranking_models = None
-        if self._ranking_features:
-            ranking_models = {
-                ranking_feature: self._get_rating_model(self._ranking_layers)
-                for ranking_feature in self._ranking_features
+    def _get_rating_models(self) -> dict[str, tf.keras.Model] | None:
+        rating_models = None
+        if self._rating_features:
+            rating_models = {
+                feature: self._get_rating_model(self._layer_sizes)
+                for feature in self._rating_features
             }
-        return ranking_models
+        return rating_models
 
     def _get_ranking_tasks(self):
         ranking_tasks = None
-        if self._ranking_features:
+        if self._rating_features:
             ranking_tasks = {
                 ranking_feature: tfrs.tasks.Ranking(
                     loss=tf.keras.losses.MeanSquaredError(),
                     metrics=[tf.keras.metrics.RootMeanSquaredError()],
                 )
-                for ranking_feature in self._ranking_features
+                for ranking_feature in self._rating_features
             }
         return ranking_tasks
 
-    @staticmethod
-    def _get_rating_model(layer_sizes) -> tf.keras.Model:
-        layers = [tf.keras.layers.Dense(num_neurons) for num_neurons in layer_sizes]
+    def _get_rating_model(self, layer_sizes) -> tf.keras.Model:
+        model = self._set_dense_layers(layer_sizes)
         if layer_sizes[-1] != 1:
-            layers.append(tf.keras.layers.Dense(1))
-        return tf.keras.Sequential(layers)
+            model.add(tf.keras.layers.Dense(1))
+        return model
