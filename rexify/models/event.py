0f8c980267f6cd99799e75e4c026a68e93209230
José Medeiros <joseprsm@gmail.com>
2022-11-27 16:16:07 +0000

    add default args

--- rexify/models/event.py
+++ rexify/models/event.py
@@ -5,9 +5,9 @@ from rexify.models.base import DenseSetterMixin
 
 
 class EventModel(tf.keras.Model, DenseSetterMixin):
-    def __init__(self, layer_sizes: list[int], n_dims: int):
+    def __init__(self, layer_sizes: list[int] = None, n_dims: int = 1):
         super().__init__()
-        self._layer_sizes = layer_sizes
+        self._layer_sizes = layer_sizes or [64, 32]
         self._n_dims = n_dims
 
         self.hidden_layers = self._set_dense_layers(layer_sizes)
