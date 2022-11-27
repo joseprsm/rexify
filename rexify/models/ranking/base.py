dbf42a53623e638d4c0774c283c856b85ef7f32e
José Medeiros <joseprsm@gmail.com>
2022-11-27 18:09:27 +0000

    add base ranking model

---
+++ rexify/models/ranking/base.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import tensorflow_recommenders as tfrs
+
+
+class BaseRankingModel(tf.keras.Model):
+
+    output_layer: tf.keras.layers.Dense
+    task: tfrs.tasks.Ranking
+
+    def __init__(self, layer_sizes: list[int]):
+        self._layer_sizes = layer_sizes or [64, 32]
+        self.hidden_layers = self._set_dense_layers(self._layer_sizes)
+
+    def call(self, inputs, labels):
+        x = self._call_layers(self.hidden_layers, inputs)
+        x = self.output_layer(x)
+        return self.task(labels=labels, predictions=x)
--- rexify/models/ranking/event.py
+++ rexify/models/ranking/event.py
@@ -1,23 +1,15 @@
 import tensorflow as tf
 import tensorflow_recommenders as tfrs
 
-from rexify.models.base import DenseSetterMixin
+from rexify.models.ranking.base import BaseRankingModel
 
 
-class EventModel(tf.keras.Model, DenseSetterMixin):
+class EventModel(BaseRankingModel):
     def __init__(self, layer_sizes: list[int] = None, n_dims: int = 1):
-        super().__init__()
-        self._layer_sizes = layer_sizes or [64, 32]
+        super().__init__(layer_sizes=layer_sizes)
         self._n_dims = n_dims
-
-        self.hidden_layers = self._set_dense_layers(layer_sizes)
         self.output_layer = tf.keras.layers.Dense(self._n_dims, activation="softmax")
         self.task = tfrs.tasks.Ranking(loss=tf.keras.losses.CategoricalCrossentropy())
-
-    def call(self, inputs, labels):
-        x = self._call_layers(self.hidden_layers, inputs)
-        x = self.output_layer(x)
-        return self.task(labels=labels, predictions=x)
 
     def get_config(self):
         return {
