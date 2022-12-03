28d4cfbac94b527405ac1bbeca62f2d23488b9a7
José Medeiros <joseprsm@gmail.com>
2022-12-03 14:05:23 +0000

    create user, item encoder

--- rexify/features/transformer.py
+++ rexify/features/transformer.py
@@ -1,4 +1,3 @@
-from sklearn.base import TransformerMixin
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
@@ -7,42 +6,38 @@ from rexify.features.transform import CategoricalEncoder, NumericalEncoder
 from rexify.types import Schema
 
 
-class FeatureTransformer(ColumnTransformer, HasSchemaInput):
-    def __init__(self, schema: Schema, use_sequential: bool = True, **kwargs):
-        self._use_sequential = use_sequential
+class _FeatureTransformer(ColumnTransformer, HasSchemaInput):
+
+    _target: str
+
+    def __init__(self, schema: Schema):
         HasSchemaInput.__init__(self, schema=schema)
         transformers = self._get_transformers()
         ColumnTransformer.__init__(
             self, transformers=transformers, remainder="passthrough"
         )
 
-    def _get_transformers(self) -> list:
-        transformer_list: list[tuple[str, TransformerMixin, list[str]]] = [
-            *self._get_features_transformers(target="user"),
-            *self._get_features_transformers(target="item"),
-        ]
-
-        transformer_list += (
-            [*self._get_features_transformers(target="context")]
-            if "context" in self.schema.keys()
-            else []
-        )
-
-        return transformer_list
-
-    def _get_features_transformers(
-        self, target: str
-    ) -> list[tuple[str, Pipeline, list[str]]]:
+    def _get_transformers(self) -> list[tuple[str, Pipeline, list[str]]]:
         transformer_list = []
 
-        cat_encoder = CategoricalEncoder(self.schema, target).as_tuple()
+        cat_encoder = CategoricalEncoder(self.schema, self.target).as_tuple()
         transformer_list += [cat_encoder] if cat_encoder[-1] != tuple() else []
 
-        num_encoder = NumericalEncoder(self.schema, target).as_tuple()
+        num_encoder = NumericalEncoder(self.schema, self.target).as_tuple()
         transformer_list += [num_encoder] if num_encoder[-1] != tuple() else []
 
         return transformer_list
 
     @property
-    def use_sequential(self):
-        return self._use_sequential
+    def target(self):
+        return self._target
+
+
+class UserTransformer(_FeatureTransformer):
+
+    _target = "user"
+
+
+class ItemTransformer(_FeatureTransformer):
+
+    _target = "item"
