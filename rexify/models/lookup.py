72cb98838c4bfac79976c73b47c3dac18f2b4720
José Medeiros <joseprsm@gmail.com>
2022-12-03 14:05:58 +0000

    add embedding lookup model

---
+++ rexify/models/lookup.py
@@ -0,0 +1,30 @@
+import numpy as np
+import tensorflow as tf
+
+
+class EmbeddingLookup(tf.keras.Model):
+    def __init__(
+        self,
+        identifiers: np.ndarray,
+        embeddings: np.ndarray,
+    ):
+        super().__init__()
+        self._identifiers = identifiers.reshape(-1)
+        self._embeddings = embeddings
+
+        identifiers_idx = np.arange(0, self._identifiers.shape[0])
+        init = tf.lookup.KeyValueTensorInitializer(
+            keys=self._identifiers, values=identifiers_idx
+        )
+
+        self.token_to_id = tf.lookup.StaticHashTable(
+            init, default_value=len(identifiers)
+        )
+
+    @tf.function(input_signature=[tf.TensorSpec([None], tf.int64)])
+    def call(self, inputs):
+        ids = self.token_to_id.lookup(inputs)
+        return tf.nn.embedding_lookup(params=self._embeddings, ids=ids)
+
+    def get_config(self):
+        return {"identifiers": self._identifiers, "embeddings": self._embeddings}
