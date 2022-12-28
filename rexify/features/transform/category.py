81572d317853913a5b1ed281d4e5ba7e03e9fd17
José Medeiros <joseprsm@gmail.com>
2022-12-28 19:41:20 +0000

    fix data types

--- rexify/features/transform/category.py
+++ rexify/features/transform/category.py
@@ -7,5 +7,5 @@ from rexify.schema import Schema
 
 class CategoricalEncoder(BaseEncoder):
     def __init__(self, schema: Schema, target: str):
-        super().__init__(dtype="categorical", target=target, schema=schema)
+        super().__init__(dtype="category", target=target, schema=schema)
         self.ppl = make_pipeline(OneHotEncoder(sparse=False))
--- rexify/features/transform/number.py
+++ rexify/features/transform/number.py
@@ -7,5 +7,5 @@ from rexify.schema import Schema
 
 class NumericalEncoder(BaseEncoder):
     def __init__(self, schema: Schema, target: str):
-        super().__init__(dtype="numerical", target=target, schema=schema)
+        super().__init__(dtype="number", target=target, schema=schema)
         self.ppl = make_pipeline(MinMaxScaler(feature_range=(-1, 1)))
