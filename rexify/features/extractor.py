f1f16446be3819c22294968249233ac62f5ef809
José Medeiros <joseprsm@gmail.com>
2023-01-03 18:46:44 +0000

    fix load order

--- rexify/features/extractor.py
+++ rexify/features/extractor.py
@@ -13,48 +13,6 @@ from rexify.schema import Schema
 from rexify.utils import get_target_id
 
 
-class _TFDatasetGenerator(HasSchemaMixin):
-    def make_dataset(self, X: pd.DataFrame) -> tf.data.Dataset:
-        ds = self._get_dataset(X)
-        ds = ds.map(self._get_header_fn())
-        return ds
-
-    def _get_dataset(self, data: pd.DataFrame) -> tf.data.Dataset:
-        return tf.data.Dataset.zip(
-            (
-                self._get_target_vector_dataset(data, self._schema, "user"),
-                self._get_target_vector_dataset(data, self._schema, "item"),
-                tf.data.Dataset.from_tensor_slices(
-                    np.stack(data["history"].values).astype(np.int32)
-                ),
-                tf.data.Dataset.from_tensor_slices(
-                    np.stack(data[self._schema.event_type].values).astype(np.float32)
-                ),
-            )
-        )
-
-    @staticmethod
-    def _get_target_vector_dataset(
-        data, schema: Schema, target: str
-    ) -> tf.data.Dataset:
-        return tf.data.Dataset.from_tensor_slices(
-            data.loc[:, get_target_id(schema, target)]
-            .values.reshape(-1)
-            .astype(np.int32)
-        )
-
-    @staticmethod
-    def _get_header_fn():
-        def header_fn(user_id, item_id, history, event):
-            return {
-                "query": {"user_id": user_id, "history": history},
-                "candidate": {"item_id": item_id},
-                "event": event,
-            }
-
-        return header_fn
-
-
 class _EventGenerator(BaseEstimator, TransformerMixin, HasSchemaMixin):
 
     _user_id: list[str]
@@ -135,6 +93,53 @@ class _EventGenerator(BaseEstimator, TransformerMixin, HasSchemaMixin):
     @property
     def ranking_features(self):
         return self._ppl.steps[0][1].ranking_features
+
+
+class _TFDatasetGenerator(HasSchemaMixin):
+    _event_gen: _EventGenerator
+
+    def make_dataset(self, X: pd.DataFrame) -> tf.data.Dataset:
+        ds = self._get_dataset(X)
+        ds = ds.map(self._get_header_fn())
+        return ds
+
+    def _get_dataset(self, data: pd.DataFrame) -> tf.data.Dataset:
+        return tf.data.Dataset.zip(
+            (
+                self._get_target_vector_dataset(data, self._schema, "user"),
+                self._get_target_vector_dataset(data, self._schema, "item"),
+                tf.data.Dataset.from_tensor_slices(
+                    np.stack(data["history"].values).astype(np.int32)
+                ),
+                tf.data.Dataset.from_tensor_slices(
+                    np.stack(data[self._schema.event_type].values).astype(np.float32)
+                ),
+            )
+        )
+
+    @staticmethod
+    def _get_target_vector_dataset(
+        data, schema: Schema, target: str
+    ) -> tf.data.Dataset:
+        return tf.data.Dataset.from_tensor_slices(
+            data.loc[:, get_target_id(schema, target)]
+            .values.reshape(-1)
+            .astype(np.int32)
+        )
+
+    def _get_ranking_dataset(self, data):
+        raise NotImplementedError
+
+    @staticmethod
+    def _get_header_fn():
+        def header_fn(user_id, item_id, history, event):
+            return {
+                "query": {"user_id": user_id, "history": history},
+                "candidate": {"item_id": item_id},
+                "event": event,
+            }
+
+        return header_fn
 
 
 class FeatureExtractor(
