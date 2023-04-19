d7b69f05afc8ccd65c7a462a5dcdc7e68e8df4d8
José Medeiros <joseprsm@gmail.com>
2023-04-19 12:46:32 +0100

    update deprecated argument

--- rexify/features/transform/category.py
+++ rexify/features/transform/category.py
@@ -8,4 +8,4 @@ from rexify.schema import Schema
 class CategoricalEncoder(BaseEncoder):
     def __init__(self, schema: Schema, target: str):
         super().__init__(dtype="category", target=target, schema=schema)
-        self.ppl = make_pipeline(OneHotEncoder(sparse=False))
+        self.ppl = make_pipeline(OneHotEncoder(sparse_output=False))
