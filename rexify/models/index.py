eb07a469c5432eccba589d4b213743e9d1f0a352
José Medeiros <joseprsm@gmail.com>
2023-04-19 13:29:57 +0100

    add base index class, scann

--- rexify/models/index.py
+++ rexify/models/index.py
@@ -2,15 +2,8 @@ import tensorflow as tf
 import tensorflow_recommenders as tfrs
 
 
-class BruteForceIndex(tfrs.layers.factorized_top_k.BruteForce):
-    def __init__(
-        self,
-        query_model: tf.keras.Model,
-        window_size,
-        k: int = 2,
-        name: str = None,
-    ):
-        super().__init__(query_model, k, name)
+class _BaseIndex:
+    def __init__(self, window_size: int):
         self._window_size = window_size
 
     def call(self, queries: tf.Tensor, k: int = None):
@@ -21,4 +14,47 @@ class BruteForceIndex(tfrs.layers.factorized_top_k.BruteForce):
                 shape=(queries_shape, self._window_size), dtype=tf.int32
             ),
         }
-        return tfrs.layers.factorized_top_k.BruteForce.call(self, inputs, k)
+        return self.__class__.__bases__[1].call(self, inputs, k)
+
+
+class BruteForceIndex(_BaseIndex, tfrs.layers.factorized_top_k.BruteForce):
+    def __init__(
+        self,
+        query_model: tf.keras.Model,
+        window_size: int,
+        k: int = 2,
+        name: str = None,
+    ):
+        tfrs.layers.factorized_top_k.BruteForce.__init__(self, query_model, k, name)
+        _BaseIndex.__init__(self, window_size)
+
+
+class ScaNN(_BaseIndex, tfrs.layers.factorized_top_k.ScaNN):
+    def __init__(
+        self,
+        query_model: tf.keras.Model,
+        window_size: int,
+        k: int = 10,
+        distance_measure: str = "dot_product",
+        num_leaves: int = 100,
+        num_leaves_to_search: int = 10,
+        training_iterations: int = 12,
+        dimensions_per_block: int = 2,
+        num_reordering_candidates: int = None,
+        parallelize_batch_searches: bool = True,
+        name: str = None,
+    ):
+        tfrs.layers.factorized_top_k.ScaNN.__init__(
+            self,
+            query_model,
+            k,
+            distance_measure,
+            num_leaves,
+            num_leaves_to_search,
+            training_iterations,
+            dimensions_per_block,
+            num_reordering_candidates,
+            parallelize_batch_searches,
+            name,
+        )
+        _BaseIndex.__init__(self, window_size)
