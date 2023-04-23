7776174410fd96b046e59c08a66caeb644fa25f3
José Medeiros <joseprsm@gmail.com>
2023-04-23 20:06:32 +0100

    move all methods to Output class

--- rexify/data/output.py
+++ rexify/data/output.py
@@ -1,20 +1,48 @@
+import json
 from pathlib import Path
 
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 
 from rexify.data.base import BaseDataFrame
 from rexify.schema import Schema
-from rexify.utils import get_target_id
+from rexify.utils import get_target_id, make_dirs
 
 
 class Output(BaseDataFrame):
+    def __init__(self, data: pd.DataFrame) -> None:
+        super().__init__(data)
+
     @classmethod
     def load(cls, path: str | Path):
-        pass
+        path = Path(path)
 
-    def save(self, path: str | Path):
-        pass
+        history = pd.read_csv(path / "history.csv")
+        features = pd.read_csv(path / "features.csv")
+        features["history"] = history.values.tolist()
+        del history
+
+        schema = Schema.from_json(path / "schema.json")
+        with open(path / "ranks.json", "r") as f:
+            ranking_features = json.load(f)
+
+        return cls(features, schema=schema, ranking_features=ranking_features)
+
+    def save(self, path: str | Path, name: str = None):
+        path = Path(path)
+        path = path / name if name else path
+
+        history = pd.DataFrame(np.stack(self.loc[:, "history"].values))
+
+        make_dirs(path)
+        history.to_csv(path / "history.csv", index=None)
+        self.drop("history", axis=1).to_csv(path / "features.csv", index=None)
+
+        with open(path / "ranks.json", "w") as f:
+            json.dump(self._ranking_features, f)
+
+        self.schema.save(path / "schema.json")
 
     def to_dataset(self) -> tf.data.Dataset:
         return self._make_dataset().map(self._get_header_fn())
