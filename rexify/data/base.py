9e8f52dfa6130552ff360bae7dc915cc532dfc6b
José Medeiros <joseprsm@gmail.com>
2023-04-23 20:07:34 +0100

    fix schema import

--- rexify/data/base.py
+++ rexify/data/base.py
@@ -4,17 +4,14 @@ from pathlib import Path
 import pandas as pd
 
 from rexify.features.base import HasSchemaMixin
+from rexify.schema import Schema
 
 
 class BaseDataFrame(pd.DataFrame, HasSchemaMixin):
-    def __init__(self, data: pd.DataFrame) -> None:
+    def __init__(self, data: pd.DataFrame, schema: Schema) -> None:
         pd.DataFrame.__init__(self, data)
+        HasSchemaMixin.__init__(self, schema=schema)
 
     @abstractmethod
-    @classmethod
     def load(cls, path: str | Path, **kwargs):
-        pass
-
-    @abstractmethod
-    def save(self, path: str | Path):
         pass
